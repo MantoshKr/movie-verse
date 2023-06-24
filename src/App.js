84bde322f945e65adbcffeb62560320a36e55c0d
mantoshdev <123961664+mantoshdev2@users.noreply.github.com>
2023-06-24 22:45:06 +0530

    Signup page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react'
 // import { Route , Routes } from 'routes'--> this is your mistake 
 import Home from './Pages/Home';
 import { BrowserRouter , Route , Routes } from 'react-router-dom';
+import Signup from './Pages/Signup';
+import Login from './Pages/Login';
+import Account from './Pages/Account';
+
 
 const App = () => {
   return (
@@ -9,6 +13,9 @@ const App = () => {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />}/>
+      <Route path="/Signup" element={<Signup />}/>
+      <Route path="/Login" element={<Login />}/>
+      <Route path="/Account" element={<Account />}/>
     </Routes>
     </BrowserRouter>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 
 
@@ -8,7 +9,9 @@ function Navbar() {
     <h1 className='text-yellow-500 text-3xl font-bold cursor-pointer   '>MovieVerse</h1>
     <div className='flex'>
         <button className='text-white p-2 font-bold  rounded hover:bg-slate-600 '>LOGIN</button>
+        <Link to='/signup'>
         <button className='bg-indigo-600 text-white font-bold p-2 rounded cursor-pointer hover:bg-indigo-700' >SIGN UP</button>
+        </Link>
     </div>
     </div>
   )

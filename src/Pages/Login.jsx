f6f6c9782abb6ce2d1a56210002d89b055fd4bc2
mantoshdev <123961664+mantoshdev2@users.noreply.github.com>
2023-06-25 14:32:11 +0530

    login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,72 @@
 import React from 'react'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
+
+
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const Navigate = useNavigate();
+  const { user ,logIn } = UserAuth();
+   
+  
+  
+  const handleSubmit = async (e) => {
+      e.preventDefault('')
+      try {
+          await logIn(email,password)
+          Navigate('/')
+      } catch (error) {
+          console.log(error)
+      
+      }
+  };
+  
+
+return (
+  <>
+  <div  className='text-gray-400 relative flex items-center '>
+      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/f841d4c7-10e1-40af-bcae-07a3f8dc141a/f6d7434e-d6de-4185-a6d4-c77a2d08737b/US-en-20220502-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="MovieVerse Logo"/>
+
+      <div className='flex flex-col p-4 absolute bg-black left-96 text-center'>
+          <h1>Sign In</h1>
+          <form onSubmit={handleSubmit}>
+              <input onChange={
+                  (e) => {
+                      setEmail(e.target.value)
+                  }
+
+              }
+              
+              type="email" placeholder="Email" />
+              <input onChange={
+                  (e) => {
+                      setPassword(e.target.value)
+                  }
+                  
+              } 
+              type="password" placeholder="Password" />
+              <button type="submit" className='bg-slate-500'>Sign In</button>
+
+          </form>
+          <div className='flex flex-row gap-4'>
+              <p>
+              <input type="checkbox"/>
+              remember me</p>
+              <p>need help?</p>
+          </div>
+          <div className='flex flex-row gap-4'>
+          <p >New to MovieVerse?</p>
+          <Link to='/Signup'>
+          <p>sign Up</p>
+          </Link>
+          </div>
+      </div>
+  </div>
+  </>
+)
 }
 
 export default Login
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,9 @@ function Navbar() {
     <div className='fixed flex item-center justify-between p-1 z-[100] w-full'>
     <h1 className='text-yellow-500 text-3xl font-bold cursor-pointer   '>MovieVerse</h1>
     <div className='flex'>
+    <Link to='/login'>
         <button className='text-white p-2 font-bold  rounded hover:bg-slate-600 '>LOGIN</button>
+        </Link>
         <Link to='/signup'>
         <button className='bg-indigo-600 text-white font-bold p-2 rounded cursor-pointer hover:bg-indigo-700' >SIGN UP</button>
         </Link>
